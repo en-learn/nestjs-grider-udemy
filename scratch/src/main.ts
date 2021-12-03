4cc4e1cdd4bd22bdd668c692aa2067882d729864
Elias Norrby <elias.norrby@gmail.com>
2021-12-03 12:17:10 +0100

    L7: File Naming Conventions

--- scratch/src/main.ts
+++ scratch/src/main.ts
@@ -1,18 +1,6 @@
-import { Controller, Get, Module } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
-@Controller()
-class AppController {
-  @Get()
-  getRootRoute() {
-    return 'hi there!'
-  }
-}
-
-@Module({
-  controllers: [AppController],
-})
-class AppModule {}
+import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
