f8d1ec981b470402caa6ddd6d4ebc5270aaed76c
Elias Norrby <elias.norrby@gmail.com>
2021-12-04 01:16:13 +0100

    L24: Manual Testing of the Controller

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,20 +1,29 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+
+  messagesService: MessagesService
+
+  constructor() {
+    // DONT DO THIS
+    this.messagesService = new MessagesService()
+  }
+
   @Get()
   listMessages() {
-
+    return this.messagesService.findAll()
   }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body)
+    return this.messagesService.create(body.content)
   }
 
   @Get(':id')
   getMessage(@Param('id') id: string) {
-    console.log(id)
+    return this.messagesService.findOne(id)
   }
 }
