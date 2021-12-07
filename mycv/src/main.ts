7dcc92c8f3088a4a7425ce83d281b76a60b417cb
Elias Norrby <elias.norrby@gmail.com>
2021-12-07 10:38:18 +0100

    L48: Manual Route Testing

--- mycv/src/main.ts
+++ mycv/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(
     new ValidationPipe({
+      // Strips extraneous properties
       whitelist: true,
     }),
   )
