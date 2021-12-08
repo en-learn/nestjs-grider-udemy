8c068ff24f8fe20e39eb6e40d225409a4613d87f
Elias Norrby <elias.norrby@gmail.com>
2021-12-08 08:59:35 +0100

    L54: Removing Users

--- mycv/src/users/users.service.ts
+++ mycv/src/users/users.service.ts
@@ -30,5 +30,11 @@ export class UsersService {
     return this.repo.save(user)
   }
 
-  remove() {}
+  async remove(id: number) {
+    const user = await this.findOne(id)
+    if (!user) {
+      throw new Error('user not found')
+    }
+    return this.repo.remove(user)
+  }
 }
