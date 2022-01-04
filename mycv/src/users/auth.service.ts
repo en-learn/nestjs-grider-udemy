26d27332ffbf4cec5ccac078a243383ba9a57330
Elias Norrby <elias.norrby@gmail.com>
2022-01-04 08:33:33 +0100

    L69: Implementing Signup Functionality

--- mycv/src/users/auth.service.ts
+++ mycv/src/users/auth.service.ts
@@ -1,7 +1,25 @@
-import { Injectable } from "@nestjs/common";
-import { UsersService } from "./users.service";
+import { BadRequestException, Injectable } from "@nestjs/common"
+import { UsersService } from "./users.service"
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService) { }
+
+  async signup(email: string, password: string) {
+    // see if email is in use
+    const users = await this.usersService.find(email)
+    if (users.length) {
+      throw new BadRequestException('email in use')
+    }
+
+    // hash the users password
+
+    // create a new user and save it
+
+    // return the user
+  }
+
+  signin() {
+
+  }
 }
