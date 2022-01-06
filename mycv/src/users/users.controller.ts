a0b33e84dc73dad85441ce37b5217a887cee9a2e
Elias Norrby <elias.norrby@gmail.com>
2022-01-06 15:12:56 +0100

    L76: Signing in a User

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common'
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Session } from '@nestjs/common'
 import { Serialize } from 'src/interceptors/serialize.interceptor'
 import { AuthService } from './auth.service'
 import { CreateUserDto } from './dtos/create-user.dto'
@@ -12,13 +12,17 @@ export class UsersController {
   constructor(private usersService: UsersService, private authService: AuthService) { }
 
   @Post('signup')
-  createUser(@Body() body: CreateUserDto) {
-    return this.authService.signup(body.email, body.password)
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password)
+    session.userId = user.id
+    return user
   }
 
   @Post('signin')
-  signin(@Body() body: CreateUserDto) {
-    return this.authService.signin(body.email, body.password)
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password)
+    session.userId = user.id
+    return user
   }
 
   @Get(':id')
