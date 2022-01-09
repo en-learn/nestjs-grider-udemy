d5ef9e3e6a7b50bd0d2a412f94fc2e7f616daacc
Elias Norrby <elias.norrby@gmail.com>
2022-01-09 10:53:13 +0100

    L92: Improving File Layout

--- mycv/src/users/auth.service.spec.ts
+++ mycv/src/users/auth.service.spec.ts
@@ -3,25 +3,31 @@ import { AuthService } from "./auth.service"
 import { User } from "./user.entity"
 import { UsersService } from "./users.service"
 
-it('can create an instance of auth service', async () => {
-  // create a fake copy of the users service
-  const fakeUsersService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password } as User)
-  }
+describe('AuthService', () => {
+  let service: AuthService
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUsersService
-      }
-    ]
-  }).compile()
+  beforeEach(async () => {
+    // create a fake copy of the users service
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User)
+    }
 
-  const service = module.get(AuthService)
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService
+        }
+      ]
+    }).compile()
 
-  expect(service).toBeDefined()
+    service = module.get(AuthService)
+  })
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined()
+  })
 })
