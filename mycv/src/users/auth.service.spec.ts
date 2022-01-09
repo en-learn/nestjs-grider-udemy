46a6e7e1e637d9645c340a02b24cab42abdd056a
Elias Norrby <elias.norrby@gmail.com>
2022-01-09 11:18:37 +0100

    L95: Testing the Signin Flow

--- mycv/src/users/auth.service.spec.ts
+++ mycv/src/users/auth.service.spec.ts
@@ -1,4 +1,4 @@
-import { BadRequestException } from "@nestjs/common"
+import { BadRequestException, NotFoundException } from "@nestjs/common"
 import { Test } from "@nestjs/testing"
 import { AuthService } from "./auth.service"
 import { User } from "./user.entity"
@@ -43,8 +43,13 @@ describe('AuthService', () => {
   })
 
   it('throws an error if user signs up with email that is in use', async () => {
-    // expect.assertions(1)
+    expect.assertions(1)
     fakeUsersService.find = () => Promise.resolve([{ id: 1, email: 'a', password: '1' } as User])
     await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toBeInstanceOf(BadRequestException)
+  })
+
+  it('throws if signin is called with an unused email', async () => {
+    expect.assertions(1)
+    await expect(service.signin('asdf@asdf.com', 'asdf')).rejects.toBeInstanceOf(NotFoundException)
   })
 })
