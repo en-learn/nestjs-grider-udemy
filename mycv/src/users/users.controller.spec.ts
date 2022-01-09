6059289a716e62eadf7aef4b10209c98bc817afd
Elias Norrby <elias.norrby@gmail.com>
2022-01-09 12:16:42 +0100

    L101: Not Super Effective Tests

--- mycv/src/users/users.controller.spec.ts
+++ mycv/src/users/users.controller.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common'
 import { Test, TestingModule } from '@nestjs/testing'
 import { AuthService } from './auth.service'
 import { User } from './user.entity'
@@ -42,5 +43,26 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined()
+  })
+
+  describe('findAllUsers', () => {
+    it('returns a list of users with the given email', async () => {
+      const users = await controller.findAllUsers('asdf@asdf.com')
+      expect(users.length).toEqual(1)
+      expect(users[0].email).toEqual('asdf@asdf.com')
+    })
+  })
+
+  describe('findUser', () => {
+    it('returns a single user with the given id', async () => {
+      const user = await controller.findUser('1')
+      expect(user).toBeDefined()
+    })
+
+    it('throws an error if user with given id is not found', async () => {
+      fakeUsersService.findOne = () => Promise.resolve(null)
+      expect.assertions(1)
+      await expect(controller.findUser('1')).rejects.toBeInstanceOf(NotFoundException)
+    })
   })
 })
