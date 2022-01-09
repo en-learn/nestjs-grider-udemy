521b728bec7437a60f211f8c3052c641c6a7b592
Elias Norrby <elias.norrby@gmail.com>
2022-01-09 11:15:14 +0100

    L94: Changing Mock Implementations

--- mycv/src/users/auth.service.spec.ts
+++ mycv/src/users/auth.service.spec.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from "@nestjs/common"
 import { Test } from "@nestjs/testing"
 import { AuthService } from "./auth.service"
 import { User } from "./user.entity"
@@ -5,10 +6,11 @@ import { UsersService } from "./users.service"
 
 describe('AuthService', () => {
   let service: AuthService
+  let fakeUsersService: Partial<UsersService>
 
   beforeEach(async () => {
     // create a fake copy of the users service
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User)
@@ -38,5 +40,11 @@ describe('AuthService', () => {
     const [salt, hash] = user.password.split('.')
     expect(salt).toBeDefined()
     expect(hash).toBeDefined()
+  })
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    // expect.assertions(1)
+    fakeUsersService.find = () => Promise.resolve([{ id: 1, email: 'a', password: '1' } as User])
+    await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toBeInstanceOf(BadRequestException)
   })
 })
