ac989fd248194febbd9de2ce8be6e490d8ad174e
Elias Norrby <elias.norrby@gmail.com>
2022-01-09 10:48:14 +0100

    L90: Getting TypeScript to Help With Mocks

--- mycv/src/users/auth.service.spec.ts
+++ mycv/src/users/auth.service.spec.ts
@@ -1,13 +1,16 @@
 import { Test } from "@nestjs/testing"
 import { AuthService } from "./auth.service"
+import { User } from "./user.entity"
 import { UsersService } from "./users.service"
 
 it('can create an instance of auth service', async () => {
   // create a fake copy of the users service
-  const fakeUsersService = {
+  const fakeUsersService: Partial<UsersService> = {
     find: () => Promise.resolve([]),
-    create: (email: string, password: string) => Promise.resolve({ id: 1, email, password })
+    create: (email: string, password: string) =>
+      Promise.resolve({ id: 1, email, password } as User)
   }
+
   const module = await Test.createTestingModule({
     providers: [
       AuthService,
